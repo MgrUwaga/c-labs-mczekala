ea983e0002d426765a405b5a6a4d4e3416670f0a
mczekala <32793245+mczekala@users.noreply.github.com>
2017-11-18 10:09:19 +0100

    Add files via upload

---
+++ 02-petle/zad6.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main ()
+{ 
+	int i,x;
+	int max=0;
+	int min=0;
+	int n[6];
+	for(i=0;i<6;i++)
+	{
+		scanf ("%d", &n[i]);
+	}
+	for(x=0;x<6;x++)
+	{
+		if(n[x]>max)
+			max=n[x];
+	}
+	min=max;
+	for(x=0;x<6;x++)
+	{
+		if(n[x]<min)
+			min=n[x];
+	}
+	printf ("max-%d\nmin-%d\n",max,min );
+}
