a410f2f7568c3133768f0881dcc2b39a2565170c
mczekala <32793245+mczekala@users.noreply.github.com>
2017-11-23 21:45:37 +0100

    Add files via upload

---
+++ zad1d.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main ()
+{
+	char string[255];
+	int i,x,z;
+	char alphabet[]={"abcdefghijklmnopqrstuvwxyzab"};
+	char alphabetU[]={"ABCDEFGHIJKLMNOPQRSTUVWXYZAB"};
+	char sign;
+	int size=0;
+	for (i=0;i<11;i++)
+	{
+	   	scanf("%c",&sign);
+	   	for(z=0;z<26;z++)
+		{
+			if(sign==alphabet[z])
+			{
+				string[size]=alphabetU[z+2];
+				size++;
+				break;
+			}
+		}
+	}
+	for(x=size-1;x>=0;x--)
+	{
+		printf("%c",string[x]);
+	}
+	return 0;
+}
