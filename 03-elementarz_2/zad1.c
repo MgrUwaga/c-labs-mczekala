77c15856e33716fb9b2aad8db607e56f608fb018
mczekala <32793245+mczekala@users.noreply.github.com>
2017-11-25 08:55:45 +0100

    Add files via upload

---
+++ 03-elementarz_2/zad1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+  int M,n,suma;
+  suma = 0;
+  n = 0;
+  printf("Podaj liczbe M: ");
+  scanf("%d", &M);
+  while (suma < M) 
+  {
+     n++;
+     suma=suma+n;
+  }
+  printf("Szukane n to: %d\n", n);
+  return 0;
+}
