a62f3c65eff329209a64f389fa979e88dcaed425
mczekala <32793245+mczekala@users.noreply.github.com>
2017-11-25 11:59:35 +0100

    Update zad4.c

--- 03-elementarz_2/zad4.c
+++ 03-elementarz_2/zad4.c
@@ -1,25 +1,28 @@
+//program zliczający znaki odstępu, tabulacji i nowego wiersza
 #include <stdio.h>
+
 int main()
 {
   char c;
   int nb = 0;  /* liczba znaków odstępu */
   int nt = 0;  /* liczba znaków tabulacji */
   int nl = 0;  /* liczba znaków nowego wiersza */
+	
   while ((c=getchar()) != EOF)
 	{
-			switch (c)
-			{
-				case 9:
-					nt++;
-					break;
-				case 10:
-					nl++;
-					break;
-				case 32:
-					nb++;
-					break;
-			}
-  }
+		switch (c)
+		{
+			case 9:
+				nt++;
+				break;
+			case 10:
+				nl++;
+				break;
+			case 32:
+				nb++;
+				break;
+		}
+  	}
   printf("liczba znaków odstępu = %d,tabulacji = %d, nowego wiersza = %d\n",nb,nt,nl);
   return 0;
 }
