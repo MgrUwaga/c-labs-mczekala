8cbe79d3659dd13ca4dd824946bdf4b9161c53df
mczekala <32793245+mczekala@users.noreply.github.com>
2017-11-25 11:42:07 +0100

    Add files via upload

--- 02-petle/zad4.c
+++ 02-petle/zad4.c
@@ -1,12 +1,20 @@
+//sumę liczb naturalnych od 1 do liczby n
 #include <stdio.h>
 
 int main ()
 { 
-	int z,n;
+	int z,n,suma;
+	suma=0;
+
 	scanf ("%d", &n);
 	
 	for(z=1;z<=n;z++)
 	{
-		printf("%d\n",z);
+		suma=suma+z;
 	}
+
+	if(n<1)
+		printf("liczba mniejsza od 1\n");
+	else
+		printf("%d\n",suma);
 }
