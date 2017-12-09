8d9146a86b2c609fec72cb85206945f9732fc7f1
Wlodek Bzyl <matwb@ug.edu.pl>
2017-12-09 10:11:20 +0100

    Poprawki w zad1d.c

--- 50-szyfrowanie/zad1d.c
+++ 50-szyfrowanie/zad1d.c
@@ -1,24 +1,47 @@
 #include <stdio.h>
+#include <string.h>
+
+// wypisz_zaszyfrowany_tekst(string) {
+// }
 
 int main() {
 	char string[255];
-	int i,x,z;
-	char alphabet[] = {"abcdefghijklmnopqrstuvwxyzab"};
-	char alphabetU[] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZAB"};
+	int i,z;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzab";
+	char alphabetU[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZAB";
 	char sign;
 	int size = 0;
+  // char text[255];
 
-	for (i=0;i<11;i++) {
-		scanf("%c",&sign);
+	// scanf("%s", text);
+	// printf("[%s]", text);
+	// strlen(text) => 11
+
+  // wczytaj_tekst(gdzie)
+	//		gdzie = fgets(gdzie, 255, stdin)
+	//    gdzie == NULL -> zakończ działanie exit(1)
+	//    gdzie != NULL -> wczytano poprawnie tekst do zaszyfrowania
+
+  // zaszyfruj(co)
+
+	for (i = 0; i < 11; i++) {
+		scanf("%c", &sign);
 		for(z=0;z<26;z++) {
-			if(sign==alphabet[z]) {
-				string[size]=alphabetU[z+2];
+			if(sign == alphabet[z]) {
+				string[size] = alphabetU[z+2];
 				size++;
 				break;
 			}
 		}
 	}
-	for(x=0;x<size;x++) {
-		printf("%c",string[x]);
-	}
+
+	// wypisz_zaszyfrowany_tekst(co)
+
+	// wypisz_zaszyfrowany_tekst(string);
+
+	// for(x = 0; x < size; x++) {
+	// 	printf("%c", string[x]);
+	// }
+	// printf("\n");
+	fputs(string, stdout);
 }
