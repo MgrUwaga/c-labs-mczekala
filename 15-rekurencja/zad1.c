5eab370be67a6ac9d9dc54515c33b6a39046f4ff
mczekala <32793245+mczekala@users.noreply.github.com>
2018-01-13 12:04:40 +0100

    Add files via upload

--- 15-rekurencja/zad1.c
+++ 15-rekurencja/zad1.c
@@ -1,1 +1,17 @@
-
+//Napisać rekurencyjną implementację funkcji obliczającej nk (n do potęgi k).
+#include <stdio.h>
+int getFactorial(int number);
+int main(int argc, char const *argv[])
+{
+  int number;
+  scanf("%d",&number);
+  printf("%d\n",getFactorial(number));
+}
+int getFactorial (int number)
+{
+  int factorial;
+  if (number<0) return 0;
+  if (number==0 || number==1) return 1;
+  factorial = number*getFactorial(number-1);
+  return factorial;
+}
