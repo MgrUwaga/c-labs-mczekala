d3bd6ae5bd94f20c232a21afebe4f58d3963dcfc
mczekala <32793245+mczekala@users.noreply.github.com>
2018-01-13 12:26:20 +0100

    Add files via upload

---
+++ 15-rekurencja/zad2.c
@@ -0,0 +1,22 @@
+//oblicza współczynnik Newtona n po k
+#include <stdio.h>
+
+int getFactorial(int number);
+int main(int argc, char const *argv[])
+{
+  binomialCoefficient();
+}
+int factorial (int number)
+{
+  int factorial;
+  if (number<0) return 0;
+  if (number==0 || number==1) return 1;
+  factorial = number*getFactorial(number-1);
+  return factorial;
+}
+int binomialCoefficient(int nValue;kValue)
+{
+  factorial(number);
+
+  return;
+}
