416b0c835affbf05fdbd39cfdb0016134fa2ecf2
mczekala <32793245+mczekala@users.noreply.github.com>
2018-01-17 17:31:19 +0100

    Add files via upload

--- 15-rekurencja/zad2.c
+++ 15-rekurencja/zad2.c
@@ -1,22 +1,25 @@
 //oblicza współczynnik Newtona n po k
 #include <stdio.h>
 
-int getFactorial(int number);
+int factorial(int number);
+int binomialCoefficient();
 int main(int argc, char const *argv[])
 {
-  binomialCoefficient();
+  int n,k;
+  scanf("%d %d",&n,&k);
+  binomialCoefficient(n,k);
 }
 int factorial (int number)
 {
-  int factorial;
+  int score;
   if (number<0) return 0;
   if (number==0 || number==1) return 1;
-  factorial = number*getFactorial(number-1);
-  return factorial;
+  score = number*factorial(number-1);
+  return score;
 }
-int binomialCoefficient(int nValue;kValue)
+int binomialCoefficient(int n,int k)
 {
-  factorial(number);
-
-  return;
+  int score = factorial(n)/(factorial(k)*factorial(n-k));
+  printf("%d",score);
+  return k;
 }
